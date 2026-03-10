ca034303b5488b4231efcc576faba25f9bb74410
R3c1ke <4kdollars@gmail.com>
2026-03-10 20:35:53 +0300

    Terraform VM provider

---
+++ terraform/modules/vm/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://terraform@100.110.30.13/system?keyfile=/home/github-runner/.ssh/github_vds"
+}
