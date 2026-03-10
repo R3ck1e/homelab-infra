1f0e7bb448798c3705b52026e66d3d252390e208
R3c1ke <4kdollars@gmail.com>
2026-03-10 11:56:01 +0300

    IKD

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,12 +1,29 @@
 resource "libvirt_network" "homelab" {
-  name = "homelab-net"
+  name      = "homelab-net"
+  autostart = true
 
-  network_xml = <<EOF
-<network>
-  <name>homelab-net</name>
-  <forward mode='nat'/>
-  <bridge name='virbr-homelab' stp='on' delay='0'/>
-  <ip address='10.10.0.1' netmask='255.255.255.0'/>
-</network>
-EOF
+  bridge {
+    name = "virbr-homelab"
+    stp  = "on"
+  }
+
+  domain {
+    name = "homelab.local"
+  }
+
+  forward {
+    mode = "nat"
+  }
+
+  ips {
+    address = "10.10.0.1"
+    netmask = "255.255.255.0"
+
+    dhcp {
+      ranges {
+        start = "10.10.0.100"
+        end   = "10.10.0.200"
+      }
+    }
+  }
 }
