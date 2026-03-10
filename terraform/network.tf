ee31520b6f7d2a7e59835b4de7dedacadbbd4ca7
R3c1ke <4kdollars@gmail.com>
2026-03-10 11:46:26 +0300

    XML-formatted network

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,15 +1,13 @@
 resource "libvirt_network" "homelab" {
-  name = "homelab-net"
-
-  mode = "nat"
-
+  name      = "homelab-net"
   autostart = true
 
-  addresses = [
-    "10.10.0.0/24"
-  ]
-
-  dhcp {
-    enabled = true
-  }
+  xml = <<EOF
+<network>
+  <name>homelab-net</name>
+  <forward mode='nat'/>
+  <bridge name='virbr-homelab' stp='on' delay='0'/>
+  <ip address='10.10.0.1' netmask='255.255.255.0'/>
+</network>
+EOF
 }
